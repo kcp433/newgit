2d055e6ba242ea21711a88d32f7c7a471a1bdde2
kcp433 <cpriyankacc@gmail.com>
2022-09-02 09:48:53 +0530

    new change 111111
    this is a test commit for checking description.
    https://github.com/kcp433
    cdtxrwretcfgvhjbbbbbbbbbbnnnnnnnnnnnnnnnn
    jjjjjjjjjjjjjjjjjjj
    kkkkkkkkkkkkkkkkkkkkkkk

--- B.java
+++ B.java
@@ -1,6 +1,6 @@
-class Hello
-{
-<<<<<<< HEAD
+class Hello {
+
+
 	public static void main(String args[]) {
 		System.out.println("Hello World");
 
@@ -8,10 +8,3 @@ class Hello
 	}
 
 }
-=======
-	public static void main(String args[])
-	{
-		System.out.println("Hello World");
-	}
-}
->>>>>>> origin/main
